77357fc39f401263e211af656184ddaa6e3a2cc7
thomas <thomas@MSI>
2016-10-28 22:39:08 +0200

    Add project files.

---
+++ AMDB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AMDB.MvcApplication" Language="C#" %>
---
+++ AMDB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AMDB.Startup))]
+namespace AMDB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
